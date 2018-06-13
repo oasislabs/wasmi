a9bf01a60f8de7bbe5c71a7d057a05f929d786a4
Sergey Pepyakin <s.pepyakin@gmail.com>
2018-06-13 16:15:45 +0300

    Working

--- src/validation/mod.rs
+++ src/validation/mod.rs
@@ -1,6 +1,6 @@
 use std::error;
 use std::fmt;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 use parity_wasm::elements::{
 	BlockType, External, GlobalEntry, GlobalType, Internal, MemoryType, Module, Opcode,
 	ResizableLimits, TableType, ValueType, InitExpr, Type
