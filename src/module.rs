dd78c8cd6db21fd36ae5156c945cec17d192e570
David Yan <david.yan@oasislabs.com>
2019-04-03 15:56:37 -0700

    Modify get_exports to only return names

--- src/module.rs
+++ src/module.rs
@@ -700,8 +700,8 @@ impl<'a> NotStartedModuleRef<'a> {
 		self.instance
 	}
 
-	pub fn get_exports(&self) -> HashMap<String, ExternVal> {
-		self.instance.exports.borrow().clone()
+	pub fn get_exports(&self) -> Vec<String> {
+		self.instance.exports.borrow().iter().map(|(key, _)| key.clone()).collect()
 	}
 }
 
